130f8e4d5e70cae422262fed18a24d92f64c2299
Danylo <10doctortennant10@gmail.com>
2023-02-22 00:43:26 +0200

    Aboba

--- DZ(17.02.23).py
+++ DZ(17.02.23).py
@@ -1,6 +1,84 @@
+# 1) *****************************
 
-name = input()
 
-x = name.capitalize()
+# name = input()
 
-print(x)
+# x = name.capitalize()
+
+# print(x)
+
+
+
+
+
+
+
+
+# 2) *****************************
+
+
+# surname = input("Youe surname pls: ")
+
+# x = list(surname)  # It can be restored by .join
+
+# print(x)
+
+
+
+
+
+
+
+# 3) *****************************
+
+
+# age_typed = input("How old are you?: ")
+
+# age = int(age_typed)
+
+# easy_way = int(2023 - age)
+
+# print("Your year of berth is:", easy_way, "or maybe +-1)")
+
+
+
+
+
+
+# 4) *****************************
+
+
+# age_typed = input("How old are you?: ")
+
+# age = int(age_typed)
+
+# if age > 15:              
+#      print("15 > is True")      
+# elif age < 15:
+#      print("15 > is False")
+
+# if age >= 23:
+#      print("23 >= is True")
+# elif age <= 23:
+#      print("23 >= is False")
+
+# if age == 13:
+#      print("13 = is True")
+# elif age != 13:
+#      print("13 = is False")
+
+
+
+
+
+
+# 5)*********************************
+
+
+this_fcking_big_txt = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium nihil laborum rerum ipsum iure dicta officiis, necessitatibus non vel excepturi minima quia fugiat hic numquam suscipit harum laboriosam magni quasi veniam voluptas eius sapiente aperiam vero? Eum ducimus consequuntur, accusamus tempore corporis numquam animi hic ad voluptatem saepe mollitia voluptate explicabo soluta earum! Consequatur porro eos minus facilis mollitia alias nesciunt. Molestias explicabo alias ea praesentium placeat ex ad maxime ipsam non velit architecto, iure laborum reiciendis pariatur dolorem amet nulla dolor quos a esse atque vel? Fuga quidem perspiciatis velit iure excepturi. Delectus velit amet distinctio error temporibus at voluptas suscipit laboriosam rerum ea, quibusdam nesciunt maiores quas necessitatibus fugit veniam, molestiae inventore dignissimos voluptatum libero enim natus! Cum, corrupti ad."
+
+# print(this_fcking_big_txt.find("ad"))
+
+# print(this_fcking_big_txt[:39])
+
+# print(this_fcking_big_txt.replace("Lorem", "Hey you"))
