9c969a1b72317ff714a1b9f2c850f3258f1934ee
Danylo <10doctortennant10@gmail.com>
2023-03-03 03:22:17 +0200

    pora spat'

---
+++ Операторы ветвлений Часть 2.py
@@ -0,0 +1,80 @@
+# Завдання 1 *****************************************************
+
+# number = input("Pls choose day of the week via number(1-7) :")
+
+# if number == "1":
+#     print("Понеділок")
+
+# if number == "2":
+#     print("Вівторок")
+
+# if number == "3":
+#     print("Середа")
+
+# if number == "4":
+#     print("Четвер")
+
+# if number == "5":
+#     print("П'ятниця")
+
+# if number == "6":
+#     print("Субота")
+
+# if number == "7":
+#     print("Неділя")
+
+
+#Завдання 2 *****************************************************
+
+# number = input("Pls choose month via number(1-12) :")
+
+# if number == "1":
+#     print("Січень")
+
+# if number == "2":
+#     print("Лютий")
+
+# if number == "3":
+#     print("Березень")
+
+# if number == "4":
+#     print("Квітень")
+
+# if number == "5":
+#     print("Травень")
+
+# if number == "6":
+#     print("Червень")
+
+# if number == "7":
+#     print("Липень")
+    
+# if number == "8":
+#     print("Серпень")
+
+# if number == "9":
+#     print("Вересень")
+
+# if number == "10":
+#     print("Жовтень")
+
+# if number == "11":
+#     print("Листопад")
+
+# if number == "12":
+#     print("Грудень")
+
+#Завдання 3 *****************************************************
+
+number_typed = input("Number pls :")
+
+number_input = int(number_typed)
+
+if number_input > 0 :
+    print("Number is positive")
+
+if number_input < 0 :
+     print("Number is negative")
+
+if number_input == 0 :
+     print("Number is equal to zero")
