c628a41a383b37aa25ec1bca1fc0094c6b514f53
Danylo <10doctortennant10@gmail.com>
2023-04-04 22:46:14 +0300

    boop

--- DZ/DZ - registration.11.03.23.py
+++ DZ/DZ - registration.11.03.23.py
@@ -1,8 +1,50 @@
-def registration(email, password) :
+import re
+
+
+def password_numbers(password) :
+    if not re.search(r'[1-9]' , password) :
+        raise TypeError("Must have number! ")
+
+def password_lenght(password) :
+    if len(password) < 6 :
+        raise TypeError("Password must have more than 6 symbols! ")
+
+
+def uppercase(password) :
+    if not re.search(r'[A-Z]', password) :
+        raise TypeError("Must have uppercase letter! ")
+
+
+def lowercase(password) :
+    if not re.search(r'[a-z]' , password) :
+        raise TypeError("Must have lowercase letter! ")
+
+
+def special_symbols_password(password) :
+    if not re.search(r'[!@#$%^&*()-_][}{};:<>?/,.`~]' , password) :
+        raise TypeError("Must have special symbols! ")
+
+
+def email_validation(email) :
+    if not re.search(r'[@.]' , email) :
+        raise TypeError("Must have @ and . ")
+
+
+def login_uppercase(login) :
+    if not login[0].isupper() : 
+        raise TypeError("Login starts from uppercase! ")
+
+def login_lenght(login) :
+    if len(login) < 2 :
+        raise TypeError("Must be more than 2 symbols! ")
+
+def registration(email, password , login) :
     return {
         'email' : email ,
-        'password' : password
+        'password' : password ,
+        'login' : login
     }
+
 
 users = []
 
@@ -19,9 +61,21 @@ while is_running:
     """).lower()
     if user_choose == 'a' :
         email = input("Your email pls: ")
-        password = input("Password (- more than 6 symbols, must contain upper and lower-case letters, must contain number , or symbols as : “@” , “!” , etc.)")
-        
-        users = registration(email, password)
+        email_validation(email)
+
+        login = input("Choose your login: ")
+        login_uppercase(login)
+        login_lenght(login)
+
+
+        password = input("Password (- more than 6 symbols, must contain upper and lower-case letters, must contain number , or symbols as : “@” , “!” , etc.)") 
+        uppercase(password)
+        lowercase(password)
+        password_lenght(password)
+        password_numbers(password)
+        special_symbols_password(password)
+
+        users = registration(email, password , login)
 
         print(users)
 
